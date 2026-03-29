12bbfcc43b40f533f0db716f7f2235dc79e27c44
shreyasgaonkar <shreyas.gaonkar@yahoo.com>
2026-03-29 11:54:58 -0700

    Fix asset paths

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ function addProjects(k, val) {
     const project = val['name'];
     const skills = val['skills'];
     const href = val['href'];
-    const img = val['img'] || '/images/SG.png';
+    const img = val['img'] || './images/SG.png';
     return ('<a href="' + href + '"><div class="box"><img src="' + img + '" alt=""><p></p><p class = "job-title">' + project + '</p> <p class = "job-dates"> ' + skills + '</p></div></a>');
 }
 
@@ -20,24 +20,24 @@ function addTestimonials(k, val) {
     const title = val['title'];
     const company = val['company'];
     const content = val['content'];
-    const img = val['img'] || '/images/SG.png';
+    const img = val['img'] || './images/SG.png';
 
     return ('<div class="box"><img src="' + img + '" alt=""><blockquote>' + content + '<span>' + name + ',<span class="job-dates"> ' + title + ' at ' + company + '</span></span></blockquote></div>');
 }
 
-$.getJSON("../data/experience.json", function(data) {
+$.getJSON("./data/experience.json", function(data) {
     $.each(data, function(key, val) {
         $('#experience').append(addExperience(key, val));
     });
 }, "jsonp");
 
-$.getJSON("../data/projects.json", function(data) {
+$.getJSON("./data/projects.json", function(data) {
     $.each(data, function(key, val) {
         $('#projects').append(addProjects(key, val))
     });
 }, "jsonp");
 
-$.getJSON("../data/testimonials.json", function(data) {
+$.getJSON("./data/testimonials.json", function(data) {
     $.each(data, function(key, val) {
         $('#testimonials').append(addTestimonials(key, val))
     });
